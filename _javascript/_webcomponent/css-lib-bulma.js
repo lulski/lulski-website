91a6629fdc1904174678818855ece12b5c61093b
lulski <milunes@gmail.com>
2020-04-26 18:59:00 +1000

    added navbar toggle state, is this how you do things in lit-html? jquery selector doesn't seem to work.

--- _javascript/_webcomponent/css-lib-bulma.js
+++ _javascript/_webcomponent/css-lib-bulma.js
@@ -5,8 +5,8 @@ import { LitElement, html } from 'lit-element';
 class CssLibBulma extends LitElement {
     render() {
         return html`
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css">   
-    `;
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css">   
+        `;
     }
 }
 
--- _javascript/_webcomponent/lulski-nav-bar.js
+++ _javascript/_webcomponent/lulski-nav-bar.js
@@ -4,59 +4,81 @@ import './css-lib-bulma.js'
 // Extend the LitElement base class
 class LulskiNavBar extends LitElement {
 
-  /**
-   * Implement `render` to define a template for your element.
-   *
-   * You must provide an implementation of `render` for any element
-   * that uses LitElement as a base class.
-   */
+  static get properties() {
+    return {
+      navbar_burger_style: { type: String },
+      navbar_menu_style: { type: String },
+      navbar_state_expanded: { type: Boolean }
+    }
+  }
+
+  constructor() {
+    super();
+
+    this.navbar_burger_style = "navbar-burger";
+    this.navbar_menu_style = "navbar-menu";
+    this.navbar_state_expanded = false;
+  }
+
+  _onBurgerClick() {
+
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+
+    if (this.navbar_state_expanded === false) {
+      this.navbar_burger_style = "navbar-burger is-active";
+      this.navbar_menu_style = ("navbar-menu is-active");
+      this.navbar_state_expanded = true;
+    } else {
+      this.navbar_burger_style = "navbar-burger";
+      this.navbar_menu_style = ("navbar-menu");
+      this.navbar_state_expanded = false;
+    }
+  }
+
+
   render() {
-    /**
-     * `render` must return a lit-html `TemplateResult`.
-     *
-     * To create a `TemplateResult`, tag a JavaScript template literal
-     * with the `html` helper function:
-     */
+
     return html`
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css">   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css">   
 
 
       <nav class="navbar" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="#">lulski website</a>
-        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-  
-      <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-start">
-          <a class="navbar-item">navbar menu 1 (home)</a>
-  
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">navbar menu 2 dropdown</a>
-            <div class="navbar-dropdown">
-              <a class="navbar-item">dropdown1 item</a>
-              <a class="navbar-item">dropdown2 item</a>
+          <div class="navbar-brand">
+            <a class="navbar-item" href="#">lulski website</a>
+            <a role="button" class=${this.navbar_burger_style} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" @click=${this._onBurgerClick}>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+      
+          <div id="navbarBasicExample" class=${this.navbar_menu_style}>
+            <div class="navbar-start">
+              <a class="navbar-item">navbar menu 1 (home)</a>
+      
+              <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">navbar menu 2 dropdown</a>
+                <div class="navbar-dropdown">
+                  <a class="navbar-item">dropdown1 item</a>
+                  <a class="navbar-item">dropdown2 item</a>
+                </div>
+              </div>
+      
             </div>
+      
+            <div class="navbar-end">
+              <div class="navbar-item">
+                <a class="button is-primary">Log in</a>
+      
+              </div>
+            </div>
+      
           </div>
-  
-        </div>
-  
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <a class="button is-primary">Log in</a>
-  
-          </div>
-        </div>
-  
-      </div>
-  
-    </nav>
+      
+      </nav>
     `;
+
   }
 }
 // Register the new element with the browser.
